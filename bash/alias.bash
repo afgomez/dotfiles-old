aeafa554425129074bc5ccf5d3d98447ca088db6
Alejandro Fernández <antarticonorte@gmail.com>
2016-11-01 00:09:02 +0100

    Add default alias.bash file from old dotfiles

---
+++ bash/alias.bash
@@ -0,0 +1,18 @@
+alias ..='cd ..'
+
+if [ `uname -s` == "Darwin"   ]; then
+  alias ls='ls -FG'
+else
+  alias ls='ls --color=yes'
+fi
+alias ll='ls -l'
+alias la='ls -A'
+
+# Augment git
+type hub >/dev/null 2>&1 && alias git='hub'
+
+# Syntaxt highlight cat
+type pygmentize >/dev/null 2>&1 && alias ccat='pygmentize -O style=monokai -f console256 -g'
+
+
+alias reload='source ~/.bash_profile'
