7bc677c64cbb885c794136425c842a93277ef474
Alejandro Fernández Gómez <a.fernandez.gomez@catawiki.nl>
2018-02-22 15:02:55 +0100

    Add neovim setup

---
+++ nvim/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+config=$HOME/.config/nvim
+
+if [ ! -d $config ]; then
+  mkdir -p $config
+fi
+
+# Manually symlink init file
+if [ ! -e $config/init.vim ]; then
+  ln -s $DOTFILES/nvim/init.vim $config/init.vim
+fi
+
+# Install vim-plug if it doesn't exist
+
+if [ ! -e ~/.local/share/nvim/site/autoload/plug.vim ]; then
+  curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
